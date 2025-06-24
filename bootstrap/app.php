8476acd2486f74fa50535a8e2d82fcacac776e37
Fawwaz Al Farisi <fawwazalfarisi.works@gmail.com>
2025-06-24 11:47:41 +0700

    fix: update middleware to web from api

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,7 +12,7 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        $middleware->api(prepend: [
+        $middleware->web(prepend: [
             EnsureFrontendRequestsAreStateful::class,
         ]);
     })
